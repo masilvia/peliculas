7a90dd86d6cccae3206fe20ac4fdcc15ed36efe4
Noel Gonzalez <noel.argonzalez@gmail.com>
2021-08-18 15:36:50 -0300

    formActor

---
+++ src/app/actores/formulario-actores/formulario-actores.component.ts
@@ -0,0 +1,32 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { EventEmitter } from '@angular/core';
+import { actorCreacionDTO } from '../actor';
+
+@Component({
+  selector: 'app-formulario-actores',
+  templateUrl: './formulario-actores.component.html',
+  styleUrls: ['./formulario-actores.component.css']
+})
+export class FormularioActoresComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder) {   }
+
+  form!: FormGroup;
+
+  @Output()
+  submit: EventEmitter<actorCreacionDTO> = new EventEmitter<actorCreacionDTO>()
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['',{ validators: [Validators.required], },],
+      fechaNacimiento: ''
+    });
+  }
+
+  onSubmit(){
+    this.submit.emit(this.form.value);
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,8 @@ import { EditarGeneroComponent } from './generos/editar-genero/editar-genero.com
 import { EditarCineComponent } from './cines/editar-cine/editar-cine.component';
 import { EditarPeliculaComponent } from './peliculas/editar-pelicula/editar-pelicula.component';
 import { FormularioGeneroComponent } from './generos/formulario-genero/formulario-genero.component';
-import { FiltroPeliculasComponent } from './peliculas/filtro-peliculas/filtro-peliculas.component'
+import { FiltroPeliculasComponent } from './peliculas/filtro-peliculas/filtro-peliculas.component';
+import { FormularioActoresComponent } from './actores/formulario-actores/formulario-actores.component'
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { FiltroPeliculasComponent } from './peliculas/filtro-peliculas/filtro-pe
     EditarCineComponent,
     EditarPeliculaComponent,
     FormularioGeneroComponent,
-    FiltroPeliculasComponent
+    FiltroPeliculasComponent,
+    FormularioActoresComponent
   ],
   imports: [
     BrowserModule,
